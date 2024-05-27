4d141d73fb3305632c7b9a4c591750589694c87d
Moons <qlowds@gmail.com>
2024-05-27 22:58:21 +0100

    💄 Add basic TitleBar button to toggle activity on

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import "./TitleBar.css";
 
 function TitleBar() {
+    const [active, setActive] = useState<boolean>(false);
+
+    const startActivity = async () => {
+        const isActive = await window.api.startActivity();
+        setActive(isActive);
+    };
+
+    const stopActivity = async () => {
+        const isActive = await window.api.stopActivity();
+        setActive(isActive);
+    };
+
     return (
         <div id="title-bar">
             <div id="title-bar-banner"></div>
@@ -8,6 +21,10 @@ function TitleBar() {
                 <div id="title-bar-button-close" className="title-bar-button" onClick={window.api.close}>⨉</div>
                 <div id="title-bar-button-minimize" className="title-bar-button" onClick={window.api.minimize}>─</div>
                 <div id="title-bar-button-help" className="title-bar-button" onClick={window.api.help}>?</div>
+                {active
+                    ? <div id="title-bar-button-stop" className="title-bar-button" onClick={stopActivity}>⬤</div>
+                    : <div id="title-bar-button-start" className="title-bar-button" onClick={startActivity}>⬤</div>
+                }
                 <div id="title-bar-button-label"></div>
             </div>
         </div>
