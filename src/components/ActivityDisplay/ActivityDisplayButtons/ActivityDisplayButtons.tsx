a0da3bf2c5ac36a6d57e058ea26a8d47b1dfe19c
Moons <qlowds@gmail.com>
2024-06-08 01:57:53 +0100

    💄 Remove tabindex from ActivityDisplay buttons

--- src/components/ActivityDisplay/ActivityDisplayButtons/ActivityDisplayButtons.tsx
+++ src/components/ActivityDisplay/ActivityDisplayButtons/ActivityDisplayButtons.tsx
@@ -9,8 +9,8 @@ type ActivityDisplayButtonsProps = {
 const ActivityDisplayButtons = (props: ActivityDisplayButtonsProps) => {
     return (
         <>
-            {props.button1 && <button type="button" className="activity-button">{props.button1.text}</button>}
-            {props.button2 && <button type="button" className="activity-button">{props.button2.text}</button>}
+            {props.button1 && <button type="button" className="activity-button" tabIndex={-1}>{props.button1.text}</button>}
+            {props.button2 && <button type="button" className="activity-button" tabIndex={-1}>{props.button2.text}</button>}
         </>
     );
 };
