d772e4c730e1d0937ba71c55970ec21500216659
Moons <qlowds@gmail.com>
2024-06-26 23:24:12 +0100

    💄 Create InputText component

---
+++ src/components/InputText/InputText.tsx
@@ -0,0 +1,65 @@
+import { type ChangeEvent } from "react";
+import "./InputText.css";
+
+const MAX_LENGTH = 999;
+
+type InputTextProps = {
+    id?: string,
+    className?: string,
+    placeholder?: string,
+    value?: string,
+    tabIndex?: number,
+    maxLength?: number,
+    limit?: boolean,
+    counter?: boolean,
+    onInput?: (value: string) => void,
+};
+
+const InputText = (props: InputTextProps) => {
+    const onInput = (e: ChangeEvent<HTMLInputElement>) => {
+        props.onInput?.(e.target.value);
+    };
+
+    return (
+        <div className="input-text">
+            <input
+                id={props.id}
+                className={props.className}
+                style={{ paddingRight: props.counter ? 32 : 8 }}
+                type="text"
+                maxLength={maxLength(props.maxLength, props.limit)}
+                placeholder={props.placeholder}
+                tabIndex={props.tabIndex ?? -1}
+                value={props.value}
+                onInput={onInput}
+            />
+            {props.counter &&
+                <div className={`input-counter ${counterValid(props.value, props.maxLength) ? "input-counter-valid" : "input-counter-invalid"}`}>
+                    {counterCount(props.value || "", props.maxLength)}
+                </div>
+            }
+        </div>
+    );
+};
+
+const maxLength = (maxLength: number = MAX_LENGTH, limit: boolean = false): number => {
+    return limit && maxLength > 0
+        ? maxLength
+        : MAX_LENGTH;
+};
+
+const counterCount = (value: string = "", max?: number): number => {
+    if (max === undefined || max <= 0)
+        return value.length;
+
+    return Math.min(max, MAX_LENGTH) - value.length;
+};
+
+const counterValid = (value: string = "", max?: number): boolean => {
+    if (max === undefined || max <= 0)
+        return true;
+
+    return value.length <= max;
+};
+
+export default InputText;
