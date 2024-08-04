c32fd9a5069ba57c2385ef86ee80f35ab832d1a7
Moons <qlowds@gmail.com>
2024-08-04 15:43:41 +0100

    🚚 Move activity.json to app userdata folder

--- src/activity/activityLoader.ts
+++ src/activity/activityLoader.ts
@@ -1,26 +1,30 @@
-import { readFileSync, writeFileSync } from "fs";
-import type Activity from "./types/Activity";
+import { app } from "electron";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { type Activity, defaultActivity } from "./types/Activity";
 import { parseActivity } from "./activityParser";
 
-const ACTIVITY_FILE: string = "activity.json";
+const ACTIVITY_FILE = `${app.getPath("userData")}/activity.json`;
 
 export const loadActivity = (): Activity | null => {
     try {
-        const activityContent = readFileSync(`./${ACTIVITY_FILE}`, { encoding: "utf-8" });
+        if (!existsSync(ACTIVITY_FILE)) {
+            saveActivity(defaultActivity);
+            return defaultActivity;
+        }
+        const activityContent = readFileSync(ACTIVITY_FILE, { encoding: "utf-8" });
         const activity = parseActivity(activityContent);
         return activity;
     }
     catch {
         // error reading file
+        return null;
     }
-
-    return null;
 };
 
 export const saveActivity = (activity: Activity) => {
     try {
         const activityContent = JSON.stringify(activity, null, 4);
-        writeFileSync(`./${ACTIVITY_FILE}`, activityContent, { encoding: "utf-8" });
+        writeFileSync(ACTIVITY_FILE, activityContent, { encoding: "utf-8" });
     }
     catch {
         // error writing file
