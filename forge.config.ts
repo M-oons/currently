7781189591509081ef2e155454ca79997ad0597a
Moons <qlowds@gmail.com>
2024-09-08 19:12:33 +0100

    💚 Remove Linux build

--- forge.config.ts
+++ forge.config.ts
@@ -2,6 +2,7 @@ import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerRpm } from "@electron-forge/maker-rpm";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
@@ -9,13 +10,12 @@ import { FuseV1Options, FuseVersion } from "@electron/fuses";
 const config: ForgeConfig = {
     packagerConfig: {
         asar: true,
-        name: "Currently",
-        executableName: "Currently",
     },
     rebuildConfig: {},
     makers: [
         new MakerSquirrel({}),
         new MakerZIP({}, ["win32", "darwin", "linux"]),
+        new MakerRpm({}),
         new MakerDeb({})
     ],
     plugins: [
