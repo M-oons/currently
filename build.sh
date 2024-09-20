1121066e16bd4bc5a31f64646080a278f81f5765
Moons <qlowds@gmail.com>
2024-09-20 23:33:32 +0100

    🔨 Update build scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing application dependencies..."
-npm install
+npm install --no-audit --no-fund
 
 if [ $? -ne 0 ]; then
     echo "Error installing application dependencies."
